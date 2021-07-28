d569a9c96937faa50963ddea16103fc083934183
jenniekibiri <josphatje@gmail.com>
2021-07-28 11:24:02 +0300

    update globalprovider

--- src/context/provider.js
+++ src/context/provider.js
@@ -2,9 +2,13 @@ import React, { createContext, useReducer } from "react";
 import category from "./reducers/category";
 import product from "./reducers/product";
 import user from "./reducers/farmer";
+import buyer from "./reducers/buyer";
+import driver from "./reducers/driver";
 import categoryInitialState from "./initialStates/categoryInitialState";
 import productInitialState from "./initialStates/productInitialState";
 import userInitialState from "./initialStates/userInitialState";
+import buyerInitialState from "./initialStates/buyerInitialState";
+import driverInitialState from "./initialStates/driverInitialState";
 export const GlobalContext = createContext({});
 export const GlobalProvider = ({ children }) => {
   const [categoryState, categoryDispatch] = useReducer(
@@ -16,6 +20,8 @@ export const GlobalProvider = ({ children }) => {
     productInitialState
   );
   const [userState, userDispatch] = useReducer(user, userInitialState);
+  const [buyerState, buyerDispatch] = useReducer(buyer, buyerInitialState);
+  const [driverState, driverDispatch] = useReducer(driver, driverInitialState);
   return (
     <GlobalContext.Provider
       value={{
