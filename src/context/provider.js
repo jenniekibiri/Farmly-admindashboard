d6c0af7013eb0984c7264a4a8a3e81844c8b3bf6
jenniekibiri <josphatje@gmail.com>
2021-07-28 13:26:12 +0300

    auth global provider

--- src/context/provider.js
+++ src/context/provider.js
@@ -4,11 +4,13 @@ import product from "./reducers/product";
 import user from "./reducers/farmer";
 import buyer from "./reducers/buyer";
 import driver from "./reducers/driver";
+import auth from "./reducers/auth";
 import categoryInitialState from "./initialStates/categoryInitialState";
 import productInitialState from "./initialStates/productInitialState";
 import userInitialState from "./initialStates/userInitialState";
 import buyerInitialState from "./initialStates/buyerInitialState";
 import driverInitialState from "./initialStates/driverInitialState";
+import authInitialState from "./intialstates/authInitialState";
 export const GlobalContext = createContext({});
 export const GlobalProvider = ({ children }) => {
   const [categoryState, categoryDispatch] = useReducer(
@@ -22,9 +24,12 @@ export const GlobalProvider = ({ children }) => {
   const [userState, userDispatch] = useReducer(user, userInitialState);
   const [buyerState, buyerDispatch] = useReducer(buyer, buyerInitialState);
   const [driverState, driverDispatch] = useReducer(driver, driverInitialState);
+  const [authState, authDispatch] = useReducer(auth, authInitialState);
   return (
     <GlobalContext.Provider
       value={{
+        authState,
+        authDispatch,
         categoryState,
         categoryDispatch,
         productState,
