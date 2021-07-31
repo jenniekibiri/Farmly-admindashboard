ac15dcc316c9cb35cd59f753e90bb5ec94e1a721
jenniekibiri <josphatje@gmail.com>
2021-07-31 15:26:57 +0300

    quick fix

--- src/variables/charts.js
+++ src/variables/charts.js
@@ -1,4 +1,8 @@
 
+const Chart = require("chart.js");
+//
+// Chart extension for making the bars rounded
+// Code from: https://codepen.io/jedtrow/full/ygRYgo
 //
 
 Chart.elements.Rectangle.prototype.draw = function () {
